f8b9a02462155981d7c4406275018abbc074f27c
ClockWize171 <jupiterminhtin998@gmail.com>
2022-06-23 22:07:31 +0630

    Added authContext for user Authentication

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
-  <ColorModeScript />
+    <ColorModeScript />
     <React.StrictMode>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </React.StrictMode>
   </ChakraProvider>
 );
